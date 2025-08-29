ab6055f3428cbf9ee1c3230671b3cdab0a0627e7
Jhon <JhonArabyan11@yandex.ru>
2025-08-29 18:52:38 +0700

    Add files via upload

--- calculator-main/main.py
+++ calculator-main/main.py
@@ -1,9 +1,10 @@
 #Импорт
 from flask import Flask, render_template, request
-
-
+import os
 app = Flask(__name__)
-
+# Настроим путь (должен быть полный) для сохранения файлов
+UPLOAD_FOLDER = 'C:/Users/Admin/Desktop/John/calculator-main/static/img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def result_calculate(size, lights, device):
     #Переменные для энергозатратности приборов
     home_coef = 100
@@ -54,13 +55,18 @@ def submit_form():
     address = request.form['address']
     email = request.form['email']
     date = request.form['date']
-    # здесь вы можете сохранить данные или отправить их по электронной почте
+    myfile = request.files['myfile']
+    # Здесь сохраняется файл в путь, сложенный из пути папки img и названия изображения
+    img_adress = myfile.filename
+    myfile.save(os.path.join(app.config['UPLOAD_FOLDER'], myfile.filename))
+    # Здесь вы можете сохранить данные или отправить их по электронной почте
     return render_template('form_result.html', 
                            #Помести переменные
                            name=name,
                            address=address,
                            email=email,
-                           date=date
+                           date=date,
+                           img_adress = img_adress
                            )
 
 app.run(debug=True)
